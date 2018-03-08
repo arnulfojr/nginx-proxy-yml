8dd1d67d052a4ccb8a58073b5f12c5b71b9858b4
Arnulfo Solis <arnulfojr94@gmail.com>
2018-03-08 19:47:07 +0100

    only register those with application name label

--- src/core/nginx/docker.py
+++ src/core/nginx/docker.py
@@ -26,8 +26,12 @@ def transform_container(container):
 
 def transform(network, me=None):
     network.reload()
+
+    def has_label(label, container):
+        return container.labels.get(label) is not None
+    
     containers = [transform_container(c)
                   for c in network.containers
-                  if c != me]
+                  if c != me and has_label(_APPLICATION_NAME_LABEL, c)]
     return containers
 
